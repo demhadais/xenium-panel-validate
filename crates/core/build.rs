a047f198833b90fadd8f55cccc7503eb8a9905f7
Ahmed Said <ahmed.said@jax.org>
2026-06-24 16:22:00 -0400

    intermeidate before building

--- crates/core/build.rs
+++ crates/core/build.rs
@@ -6,27 +6,26 @@ use std::{
 };
 
 use anyhow::Context;
-use bstr::ByteSlice;
+use bstr::{BString, ByteSlice};
 use heck::ToPascalCase;
 use noodles::gff::feature::{RecordBuf, record_buf::attributes::field::Value};
 use proc_macro2::Span;
 use quote::quote;
 use serde::Deserialize;
-use syn::Ident;
+use syn::{Ident, LitStr};
 use url::Url;
+
+const N_GENES: usize = 24_000;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     println!("cargo::rerun-if-changed=genes.toml");
-    let config = include_bytes!("genes.toml");
 
-    let Config { human, mouse } =
-        toml::from_slice(config).context("failed to parse config from genes.toml")?;
+    let Config { human, mouse } = toml::from_slice(include_bytes!("genes.toml"))
+        .context("failed to parse config from genes.toml")?;
 
-    // For now, hard-coding these 4 things is no big deal, but in the future, maybe
-    // we should refactor this into something reusable
-    let human_annotations = || read_gene_annotations(&human.gene_annotations_path);
-    let mouse_annotations = || read_gene_annotations(&mouse.gene_annotations_path);
+    let mut annotations = HashSet::with_capacity(N_GENES);
+    read_gene_annotations_into(&human.gene_annotations_path, &mut annotations)?;
 
     let http_client = reqwest::Client::new();
     let unavailable_gene_sets = [
@@ -44,27 +43,22 @@ async fn main() -> anyhow::Result<()> {
         mouse_prime_unavailable,
     ] = unavailable_gene_sets.as_array().unwrap();
 
-    let human_v1_enums = make_enums("XeniumV1Human", human_annotations()?, &human_v1_unavailable)?;
+    let human_v1_enums = make_enums("XeniumV1Human", &annotations, &human_v1_unavailable)?;
     std::fs::write("src/gene_list/gene/xenium_v1_human.rs", human_v1_enums)?;
 
-    let human_prime_enums = make_enums(
-        "XeniumPrimeHuman",
-        human_annotations()?,
-        &human_prime_unavailable,
-    )?;
+    let human_prime_enums = make_enums("XeniumPrimeHuman", &annotations, &human_prime_unavailable)?;
     std::fs::write(
         "src/gene_list/gene/xenium_prime_human.rs",
         human_prime_enums,
     )?;
 
-    let mouse_v1_enums = make_enums("XeniumV1Mouse", human_annotations()?, &mouse_v1_unavailable)?;
+    annotations.clear();
+    read_gene_annotations_into(&mouse.gene_annotations_path, &mut annotations)?;
+
+    let mouse_v1_enums = make_enums("XeniumV1Mouse", &annotations, &mouse_v1_unavailable)?;
     std::fs::write("src/gene_list/gene/xenium_v1_mouse.rs", mouse_v1_enums)?;
 
-    let mouse_prime_enums = make_enums(
-        "XeniumPrimeMouse",
-        mouse_annotations()?,
-        &mouse_prime_unavailable,
-    )?;
+    let mouse_prime_enums = make_enums("XeniumPrimeMouse", &annotations, &mouse_prime_unavailable)?;
     std::fs::write(
         "src/gene_list/gene/xenium_prime_mouse.rs",
         mouse_prime_enums,
@@ -86,18 +80,31 @@ struct SpeciesConfig {
     gene_annotations_path: PathBuf,
 }
 
-fn read_gene_annotations(path: &Path) -> anyhow::Result<GeneAnnotationFileReader> {
-    let file =
-        File::open(path).with_context(|| format!("failed to read {}", path.to_str().unwrap()))?;
-
-    Ok(noodles::gtf::io::Reader::new(std::io::BufReader::new(file)))
-}
-
 type GeneAnnotationFileReader = noodles::gtf::io::Reader<BufReader<File>>;
+
+fn read_gene_annotations_into(
+    path: &Path,
+    genes_buf: &mut HashSet<(String, String)>,
+) -> anyhow::Result<()> {
+    let mut reader = GeneAnnotationFileReader::new(BufReader::new(File::open(path)?));
+
+    for record in reader.record_bufs() {
+        let record = record?;
+
+        let Some((ensembl_id, gene_name)) = parse_ensembl_id_and_name_from_gtf_record(&record)?
+        else {
+            continue;
+        };
+
+        genes_buf.insert((ensembl_id.to_owned(), gene_name.to_owned()));
+    }
+
+    Ok(())
+}
 
 fn make_enums(
     enum_prefix: &str,
-    mut reader: GeneAnnotationFileReader,
+    genes: &HashSet<(String, String)>,
     unavailable_gene_ids: &HashSet<String>,
 ) -> anyhow::Result<String> {
     const N_GENES: usize = 30_000;
@@ -110,14 +117,29 @@ fn make_enums(
     let ensembl_id_enum_name = Ident::new(&format!("{enum_prefix}EnsemblId"), Span::call_site());
     let gene_name_enum_name = Ident::new(&format!("{enum_prefix}GeneName"), Span::call_site());
 
-    for record in reader.record_bufs() {
-        let (ensembl_id, gene_name) = parse_ensembl_id_and_name_from_gtf_record(&record?)?;
-        if unavailable_gene_ids.contains(&ensembl_id) {
+    for (ensembl_id, gene_name) in genes {
+        let ensembl_id = ensembl_id.split('.').next().unwrap();
+
+        if unavailable_gene_ids.contains(ensembl_id) {
             continue;
         }
 
-        let ensembl_id_variant = Ident::new(&ensembl_id, Span::call_site());
-        let gene_name_variant = Ident::new(&gene_name, Span::call_site());
+        let ensembl_id_variant = Ident::new(&ensembl_id.to_pascal_case(), Span::call_site());
+
+        let naive_gene_name = gene_name.replace(".", "").to_pascal_case();
+
+        let gene_name_variant = if gene_name.chars().next().unwrap().is_numeric() {
+            let gene_name_variant = Ident::new(&format!("_{naive_gene_name}"), Span::call_site());
+
+            gene_name_variant
+        } else {
+            let gene_name_variant = Ident::new(&naive_gene_name, Span::call_site());
+
+            gene_name_variant
+        };
+
+        let lowercase_gene_name = LitStr::new(&gene_name.to_lowercase(), Span::call_site());
+        let serde_annotation = quote! { #[serde(rename = #lowercase_gene_name)] };
 
         ensembl_id_to_gene_name_match_arms
             .push(quote! { Self::#ensembl_id_variant => #gene_name_enum_name::#gene_name_variant });
@@ -127,7 +149,10 @@ fn make_enums(
         );
 
         ensembl_id_enum_variants.push(ensembl_id_variant);
-        gene_name_enum_variants.push(gene_name_variant);
+        gene_name_enum_variants.push(quote! {
+            #serde_annotation
+            #gene_name_variant
+        });
     }
 
     Ok(quote! {
@@ -188,8 +213,16 @@ async fn fetch_unavailable_ensembl_ids(
 
 fn parse_ensembl_id_and_name_from_gtf_record<'a>(
     record: &'a RecordBuf,
-) -> anyhow::Result<(String, String)> {
+) -> anyhow::Result<Option<(&'a str, &'a str)>> {
+    if record.ty() != "gene" {
+        return Ok(None);
+    }
+
     let attributes = record.attributes();
+
+    if attributes.get(b"gene_type") != Some(&Value::String(BString::from("protein_coding"))) {
+        return Ok(None);
+    }
 
     let (Some(Value::String(gene_id)), Some(Value::String(gene_name))) =
         (attributes.get(b"gene_id"), attributes.get(b"gene_name"))
@@ -197,14 +230,8 @@ fn parse_ensembl_id_and_name_from_gtf_record<'a>(
         unreachable!("'gene_id' and 'gene_name' should be strings");
     };
 
-    Ok((
-        gene_id
-            .to_str()
-            .expect("Ensembl ID should be UTF8")
-            .to_pascal_case(),
-        gene_name
-            .to_str()
-            .expect("Ensembl ID should be UTF8")
-            .to_pascal_case(),
-    ))
+    Ok(Some((
+        gene_id.to_str().expect("Ensembl ID should be UTF8"),
+        gene_name.to_str().expect("Gene name should be UTF8"),
+    )))
 }
